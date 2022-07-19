2e495520bd27aa0c5f7c867cb7adf0e45b4bacbd
Paul Hooijenga <paulhooijenga@gmail.com>
2022-07-19 15:21:43 +0200

    Implement slash commands

--- cmd/dicebot/dicebot.go
+++ cmd/dicebot/dicebot.go
@@ -21,9 +21,10 @@ func logMessage(s *discordgo.Session, level int, format string, args ...interfac
 }
 
 func onReady(s *discordgo.Session, event *discordgo.Ready) {
-	logMessage(s, discordgo.LogInformational, "Received ready event: %+v", event)
+	logMessage(s, discordgo.LogDebug, "Received ready event: %+v", event)
+	logMessage(s, discordgo.LogInformational, "Logged in as %s", s.State.User)
 
-	if err := s.UpdateStatus(0, ""); err != nil {
+	if err := s.UpdateGameStatus(0, ""); err != nil {
 		logMessage(s, discordgo.LogError, "Unable to set status: %v", err)
 	}
 }
@@ -75,28 +76,134 @@ func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 	logMessage(s, discordgo.LogDebug, "Received guild event: %+v", event.Guild)
 }
 
+var commands = []discordgo.ApplicationCommand{
+	{
+		Name:        "roll",
+		Description: "Roll some dice",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "dice",
+				Description: "What dice to roll (2d6, d20+2)",
+				Required:    true,
+			},
+		},
+	},
+	{
+		Name:        "save",
+		Description: "Save a dice roll",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "dice",
+				Description: "What dice to roll (2d6, d20+2)",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "name",
+				Description: "The name for this roll",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "for",
+				Description: "Who should be able to use this roll",
+				Required:    false,
+				Choices: []*discordgo.ApplicationCommandOptionChoice{
+					{Name: "just me", Value: "user"},
+					{Name: "this channel", Value: "channel"},
+					{Name: "whole server", Value: "server"},
+				},
+			},
+		},
+	},
+}
+
+func GetUser(i *discordgo.Interaction) *discordgo.User {
+	if i.User != nil {
+		return i.User
+	}
+	if i.Member != nil {
+		return i.Member.User
+	}
+	return nil
+}
+
+func onInteractionCreate(s *discordgo.Session, event *discordgo.InteractionCreate) {
+	logMessage(s, discordgo.LogInformational, "Received interaction: %+v", event.Interaction)
+
+	channel, err := s.State.Channel(event.ChannelID)
+	if err != nil {
+		logMessage(s, discordgo.LogError, "Unable to retrieve channel info for %s: %s", event.ChannelID, err)
+		channel = &discordgo.Channel{GuildID: "unknown"}
+	}
+
+	user := GetUser(event.Interaction)
+
+	context := dicebot.MessageContext{
+		UserId:    user.ID,
+		UserName:  user.Username,
+		ChannelId: event.ChannelID,
+		ServerId:  channel.GuildID,
+	}
+
+	commandData := event.ApplicationCommandData()
+	options := make(map[string]string, len(commandData.Options))
+	for _, option := range commandData.Options {
+		options[option.Name] = option.StringValue()
+	}
+
+	var response string
+
+	if commandData.Name == "roll" {
+		dice := options["dice"]
+		response = bot.RollDice(context, dice)
+	} else if commandData.Name == "save" {
+		dice, name, for_ := options["dice"], options["name"], options["for"]
+		if err := bot.Save(context, dice, name, for_); err != nil {
+			response = bot.HandleError(fmt.Sprintf("save %v as %v", dice, name), err)
+		} else {
+			response = fmt.Sprintf("Saved **%s** as `%s`", dice, name)
+		}
+	} else {
+		logMessage(s, discordgo.LogError, "Unknown interaction command %v", commandData.Name)
+		return
+	}
+
+	err = s.InteractionRespond(event.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: response,
+		},
+	})
+	if err != nil {
+		logMessage(s, discordgo.LogError, "Unable to send interaction response: %s", err)
+	}
+}
+
 func run(context *cli.Context) error {
 	token := context.String("token")
 	if token == "" {
-		return cli.NewExitError("Authentication token is required.", 1)
+		return cli.Exit("Authentication token is required.", 1)
 	}
 
 	var err error
 	bot, err = dicebot.NewBot(context.String("database"))
 	if err != nil {
-		return cli.NewExitError(fmt.Sprintf("Unable to open database: %s", err), 1)
+		return cli.Exit(fmt.Sprintf("Unable to open database: %s", err), 1)
 	}
 
 	for _, filename := range context.StringSlice("moves") {
 		err = bot.LoadMoves(filename)
 		if err != nil {
-			return cli.NewExitError(fmt.Sprintf("Unable to load moves from %s: %s", filename, err), 1)
+			return cli.Exit(fmt.Sprintf("Unable to load moves from %s: %s", filename, err), 1)
 		}
 	}
 
 	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
-		return cli.NewExitError(fmt.Sprintf("Unable to create Discord session: %s", err), 1)
+		return cli.Exit(fmt.Sprintf("Unable to create Discord session: %s", err), 1)
 	}
 
 	switch context.String("log-level") {
@@ -113,7 +220,7 @@ func run(context *cli.Context) error {
 		discord.LogLevel = discordgo.LogDebug
 		break
 	default:
-		return cli.NewExitError(fmt.Sprintf("Unknown log level '%s'", context.String("log-level")), 1)
+		return cli.Exit(fmt.Sprintf("Unknown log level '%s'", context.String("log-level")), 1)
 	}
 
 	discord.ShardID = context.Int("shard")
@@ -123,9 +230,16 @@ func run(context *cli.Context) error {
 	discord.AddHandler(onMessageCreate)
 	discord.AddHandler(onMessageUpdate)
 	discord.AddHandler(onGuildCreate)
+	discord.AddHandler(onInteractionCreate)
 
 	if err := discord.Open(); err != nil {
-		return cli.NewExitError(fmt.Sprintf("Unable to connect to Discord: %s", err), 1)
+		return cli.Exit(fmt.Sprintf("Unable to connect to Discord: %s", err), 1)
+	}
+
+	for _, command := range commands {
+		if _, err := discord.ApplicationCommandCreate(discord.State.User.ID, "", &command); err != nil {
+			return cli.Exit(fmt.Sprintf("Unable to create command %v", command.Name), 1)
+		}
 	}
 
 	c := make(chan os.Signal, 1)
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/hackedd/dicebot
 go 1.15
 
 require (
-	github.com/bwmarrin/discordgo v0.22.0
-	github.com/urfave/cli/v2 v2.2.0
+	github.com/bwmarrin/discordgo v0.25.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli/v2 v2.11.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
